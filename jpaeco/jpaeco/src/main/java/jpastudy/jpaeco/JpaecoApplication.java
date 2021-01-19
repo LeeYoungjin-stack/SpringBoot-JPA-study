bb14b11768526d5a5911790a629a58e6a7ca01f6
Youngjin <dudwls0505@naver.com>
2021-01-19 20:29:50 +0900

    spring boot 세팅완료
    localhost hello world

---
+++ jpaeco/jpaeco/src/main/java/jpastudy/jpaeco/JpaecoApplication.java
@@ -0,0 +1,15 @@
+package jpastudy.jpaeco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaecoApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaecoApplication.class, args);
+	}
+
+}
---
+++ jpaeco/jpaeco/src/test/java/jpastudy/jpaeco/JpaecoApplicationTests.java
@@ -0,0 +1,13 @@
+package jpastudy.jpaeco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
